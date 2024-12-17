f7fe62489fef3bdb402e6e9c0fead99e0ddbd288
randrahim <rrahim3@gatech.edu>
2024-12-16 21:44:50 -0500

    Using scss instead of css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
+import "./App.scss";
 import About from "./pages/About.jsx";
 import Home from "./pages/Home.jsx";
 import RentalPropertyCard from "./pages/RentalPropertyCard";
